0c9915620fdffd2fbb592113510844e6a4696b0c
kojo <shinji_kojyou@jrass.jp>
2021-04-07 20:16:01 +0900

    20210407 Update

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by PC-SYSKAI551 on 2021/04/07.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
